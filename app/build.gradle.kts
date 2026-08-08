38447e3dbf8138ddb51ce5200cb16b687aa6d3a8
Grapaxels <nikhilkumar1338@gmail.com>
2026-08-09 00:05:19 +0530

    app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.grapaxels.mowell"
         minSdk = 24
         targetSdk = 35
-        versionCode = 34
-        versionName = "2.3.4"
+        versionCode = 35
+        versionName = "2.3.5"
         buildConfigField("boolean", "SELF_UPDATE", "true")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
